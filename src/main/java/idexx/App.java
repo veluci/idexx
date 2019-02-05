f6c4d23449acfe36e86bc2bb13e899db3a02c941
veluci <lucian.vecerdea@gmail.com>
2019-02-06 00:07:42 +0100

    clean up

--- src/main/java/idexx/App.java
+++ src/main/java/idexx/App.java
@@ -1,19 +1,11 @@
 package idexx;
 
-import idexx.converters.AlbumToItemDtoConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.support.ConversionServiceFactoryBean;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @SpringBootApplication
 @ComponentScan("idexx")
@@ -22,21 +14,8 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
-    @Autowired
-    private AlbumToItemDtoConverter albumToItemDtoConverter;
-
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
-    }
-
-    @Bean
-    public ConversionService conversionService() {
-        ConversionServiceFactoryBean factory = new ConversionServiceFactoryBean();
-        Set<Converter> converters = new HashSet<Converter>();
-        converters.add(albumToItemDtoConverter);
-        factory.setConverters(converters);
-        factory.afterPropertiesSet();
-        return factory.getObject();
     }
 }
--- src/main/java/idexx/controllers/BooksAndAlbumsController.java
+++ src/main/java/idexx/controllers/BooksAndAlbumsController.java
@@ -6,11 +6,9 @@ import idexx.dto.ResponseDto;
 import idexx.services.AlbumsService;
 import idexx.services.BooksService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class BooksAndAlbumsController {
 
