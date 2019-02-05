e23a07bec16bed2560055f6b531c8cf018798d70
veluci <lucian.vecerdea@gmail.com>
2019-02-06 00:16:53 +0100

    clean up

--- src/main/java/idexx/controllers/BooksAndAlbumsController.java
+++ src/main/java/idexx/controllers/BooksAndAlbumsController.java
@@ -8,7 +8,7 @@ import idexx.services.BooksService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin()
 @RestController
 public class BooksAndAlbumsController {
 
