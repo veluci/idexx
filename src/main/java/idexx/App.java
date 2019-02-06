ca1dac8e3a6188148251780b7331007602c3daf9
veluci <lucian.vecerdea@gmail.com>
2019-02-06 01:31:36 +0100

    cors

--- src/main/java/idexx/App.java
+++ src/main/java/idexx/App.java
@@ -6,11 +6,6 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @ComponentScan("idexx")
@@ -23,16 +18,5 @@ public class App {
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers","Access-Control-Allow-Origin","Access-Control-Request-Method", "Access-Control-Request-Headers","Origin","Cache-Control", "Content-Type", "Authorization"));
-        configuration.setAllowedMethods(Arrays.asList("DELETE", "GET", "POST", "PATCH", "PUT"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+    
 }
--- src/main/java/idexx/controllers/BooksAndAlbumsController.java
+++ src/main/java/idexx/controllers/BooksAndAlbumsController.java
@@ -6,12 +6,9 @@ import idexx.dto.ResponseDto;
 import idexx.services.AlbumsService;
 import idexx.services.BooksService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-//@CrossOrigin(origins = "https://idexx-fe.herokuapp.com:443")
+@CrossOrigin(origins = "https://albums-and-books-fe.herokuapp.com/")
 @RestController
 public class BooksAndAlbumsController {
 
